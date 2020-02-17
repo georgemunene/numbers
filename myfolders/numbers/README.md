65a726968bfe122a3e0f136acdbc28b111bf9275
georgemunene <kengeorge010@gmail.com>
2020-02-17 17:57:40 +0300

    add a function to js

---
+++ myfolders/numbers/README.md
@@ -0,0 +1,8 @@
+# ABOUT
+This program is based on front-end languages, mainly ; HTML, CSS and JAVAscript.
+
+## Authors
+georgemunene.
+
+## Technologies
+CSS, JAVASCRIPT, HTML
---
+++ myfolders/numbers/js/script.js
@@ -0,0 +1,5 @@
+function getloops(){  
+    var numb  =   document.getElementById("countto").value;
+    var numb2 =  document.getElementById("countby").value;
+    alert(numb numb2);
+    }  
